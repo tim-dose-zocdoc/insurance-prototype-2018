527cfc17186ea56857dd24f50fa0f858f2aa5a82
Tim Dosé <tim.dose@zocdoc.com>
2018-05-29 18:18:02 -0400

    load all insurance carriers and plans and display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import carriers from './data/carriers.json';
+import plans from './data/insurance_data.json';
 import './App.css';
 
 class App extends Component {
@@ -8,14 +10,38 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!!!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="carriers">
+          <PlanList plans={plans} />
+          <CarrierList carriers={carriers} />
+        </div>
       </div>
     );
   }
+}
+
+function PlanList(props) {
+  const plans = props.plans;
+  const listItems = plans.map((plan) =>
+    <li key={plan.PlanType_ID}>{plan['Carrier']} - {plan['Plan']}</li> 
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+function CarrierList(props) {
+  const carriers = props.carriers;
+  const listItems = carriers.map((carrier) =>
+    <li key={carrier.InsuranceID}>{carrier['Carrier Name']}</li> 
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
 }
 
 export default App;
