a263739ff9d357865a7a59c29d324f6a1aaad362
Tim Dosé <tim.dose@zocdoc.com>
2018-05-30 14:15:35 -0400

    more cleanup after babel

--- src/components/InsurancePicker/index.js
+++ src/components/InsurancePicker/index.js
@@ -3,12 +3,7 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
-    }
-
-    handleSearchTextChange(e) {
+    handleSearchTextChange = (e) => {
         this.props.onSearchTextChange(e.target.value);
     }
 
