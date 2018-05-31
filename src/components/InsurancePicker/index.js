dd75ea4341fae4863f6bd82a1638e574c953759a
Tim Dosé <tim.dose@zocdoc.com>
2018-05-31 09:43:35 -0400

    don't hide popular on search

--- src/components/InsurancePicker/index.js
+++ src/components/InsurancePicker/index.js
@@ -90,20 +90,6 @@ class InsurancePicker extends Component {
         return _(this.props.carriers).orderBy('requests', 'desc').slice(0,count).sortBy('name');
     }
 
-    maybeRenderPopularCarriers = () => {
-        if (this.state.searchText.length == 0 ) {
-            return (
-                <div>
-                    <h2>Popular carriers</h2>
-                    <CarrierList
-                        carriers={this.topCarriers(5)}
-                        searchText={this.state.searchText}
-                    />
-                </div>
-            );
-        }
-    }
-
     render() {
         return (
             <div>
@@ -111,9 +97,13 @@ class InsurancePicker extends Component {
                     searchText={this.state.searchText}
                     onSearchTextChange={this.handleSearchTextChange}
                 />
-                {this.maybeRenderPopularCarriers()}
+                <h2>Popular carriers</h2>
+                <CarrierList
+                    carriers={this.topCarriers(5)}
+                    searchText={this.state.searchText}
+                />
                 
-                {this.state.searchText.length == 0 ? <h2>All carriers</h2> : ''}
+                <h2>All carriers</h2>
                 <CarrierList
                     carriers={this.props.carriers}
                     searchText={this.state.searchText}
